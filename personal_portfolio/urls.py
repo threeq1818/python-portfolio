fc91aa2cd5221f486b168ef23481625014f1e9a9
Three Q <threeq1818@gmail.com>
2019-08-14 04:33:27 +0800

    added the projects_detail code

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    url('^admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     path('projects/', include('projects.urls'))
 ]
