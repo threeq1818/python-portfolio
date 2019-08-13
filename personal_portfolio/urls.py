e5798342dda75791f08fe71c440ce5a065f332bb
Three Q <threeq1818@gmail.com>
2019-08-14 03:48:50 +0800

    create and add code for Bootstrap into personal_portfolio/templates/base.html create two templates: 'projects/templates/project_detail.html', 'projects/templates/project_index.html

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url('^admin/', admin.site.urls),
+    path('projects/', include('projects.urls'))
 ]
