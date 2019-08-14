354f6f0f6d25d7e5bc1726753af2e588a71a53ce
Three Q <threeq1818@gmail.com>
2019-08-14 15:34:49 +0800

    add urls, views, forms, templates for blog project complete the project

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    path('projects/', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('blog/', include('blog.urls')),
 ]
